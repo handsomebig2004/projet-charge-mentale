53c6b43ee13aa2defd352686a41eb0b544cfd050
redzebra42 <delaigletristan42@gmail.com>
2025-12-19 11:05:13 +0100

    Normalize data in loaders

--- fcn_main.py
+++ fcn_main.py
@@ -24,7 +24,7 @@ for folder_name in os.walk("data/MAUS/Data/Raw_data/"):
         for trial in  pd.read_csv(f"{folder_name[0]}/pixart.csv").to_numpy().transpose():
             x_pix_ppg.append(list(trial.astype(np.float32)))
         for trial in pd.read_csv(f"data/MAUS/Subjective_rating/{folder_name[0][-3:]}/NASA_TLX.csv").iloc[7, 1:7].to_numpy():
-            y.append(np.float32(trial))
+            y.append(np.float32(trial)/100)
 
 # resample data to 4Hz on 30 seconds
 resample_size = 120
@@ -33,7 +33,46 @@ x_gsr_res = [resample(x, resample_size) for x in x_gsr]
 x_inf_ppg_res = [resample(x, resample_size) for x in x_inf_ppg]
 #x_pix_ppg_res = resample(x_pix_ppg, resample_size) TODO make this work (not all things of the same size)
 
-x_ecg_res_train, x_ecg_res_test, x_gsr_res_train, x_gsr_res_test, x_inf_ppg_res_train, x_inf_ppg_res_test, x_ecg_train, x_ecg_test, x_gsr_train, x_gsr_test, x_inf_ppg_train, x_inf_ppg_test, y_train, y_test = train_test_split(
+x_ecg_res_norm = torch.nn.functional.normalize(torch.tensor(x_ecg_res))
+x_gsr_res_norm = torch.nn.functional.normalize(torch.tensor(x_gsr_res))
+x_inf_ppg_norm = torch.nn.functional.normalize(torch.tensor(x_inf_ppg))
+x_ecg_norm = torch.nn.functional.normalize(torch.tensor(x_ecg))
+x_gsr_norm = torch.nn.functional.normalize(torch.tensor(x_gsr))
+x_inf_ppg_norm = torch.nn.functional.normalize(torch.tensor(x_inf_ppg))
+
+(
+x_ecg_res_norm_train,
+x_ecg_res_norm_test,
+x_gsr_res_norm_train,
+x_gsr_res_norm_test,
+x_inf_ppg_res_norm_train,
+x_inf_ppg_res_norm_test,
+x_ecg_norm_train,
+x_ecg_norm_test,
+x_gsr_norm_train,
+x_gsr_norm_test,
+x_inf_ppg_norm_train,
+x_inf_ppg_norm_test,
+x_ecg_res_train,
+x_ecg_res_test,
+x_gsr_res_train,
+x_gsr_res_test,
+x_inf_ppg_res_train,
+x_inf_ppg_res_test,
+x_ecg_train,
+x_ecg_test,
+x_gsr_train,
+x_gsr_test,
+x_inf_ppg_train,
+x_inf_ppg_test,
+y_train,
+y_test) = train_test_split(
+    x_ecg_res_norm,
+    x_gsr_res_norm,
+    x_inf_ppg_norm,
+    x_ecg_norm,
+    x_gsr_norm,
+    x_inf_ppg_norm,
     x_ecg_res,
     x_gsr_res,
     x_inf_ppg_res,
@@ -60,6 +99,22 @@ x_gsr_test_loader = torch.utils.data.DataLoader(torch.tensor(x_gsr_test), shuffl
 x_inf_ppg_train_loader = torch.utils.data.DataLoader(torch.tensor(x_inf_ppg_train), shuffle=True, batch_size=12)
 x_inf_ppg_test_loader = torch.utils.data.DataLoader(torch.tensor(x_inf_ppg_test), shuffle=True, batch_size=12)
 
+# resampled normalized data loaders
+x_ecg_res_train_loader = torch.utils.data.DataLoader(x_ecg_res_norm_train, shuffle=True, batch_size=12)
+x_ecg_res_test_loader = torch.utils.data.DataLoader(x_ecg_res_norm_test, shuffle=True, batch_size=12)
+x_gsr_res_train_loader = torch.utils.data.DataLoader((x_gsr_res_norm_train), shuffle=True, batch_size=12)
+x_gsr_res_test_loader = torch.utils.data.DataLoader((x_gsr_res_norm_test), shuffle=True, batch_size=12)
+x_inf_res_ppg_train_loader = torch.utils.data.DataLoader((x_inf_ppg_res_norm_train), shuffle=True, batch_size=12)
+x_inf_res_ppg_test_loader = torch.utils.data.DataLoader((x_inf_ppg_res_norm_test), shuffle=True, batch_size=12)
+
+# not resampled normalized data loaders
+x_ecg_train_norm_loader = torch.utils.data.DataLoader((x_ecg_norm_train), shuffle=True, batch_size=12)
+x_ecg_test_norm_loader = torch.utils.data.DataLoader((x_ecg_norm_test), shuffle=True, batch_size=12)
+x_gsr_train_norm_loader = torch.utils.data.DataLoader((x_gsr_norm_train), shuffle=True, batch_size=12)
+x_gsr_test_norm_loader = torch.utils.data.DataLoader((x_gsr_norm_test), shuffle=True, batch_size=12)
+x_inf_ppg_train_norm_loader = torch.utils.data.DataLoader((x_inf_ppg_norm_train), shuffle=True, batch_size=12)
+x_inf_ppg_test_norm_loader = torch.utils.data.DataLoader((x_inf_ppg_norm_test), shuffle=True, batch_size=12)
+
 # y data loaders
 y_train_loader = torch.utils.data.DataLoader(torch.tensor(y_train), shuffle=True, batch_size=12)
 y_test_loader = torch.utils.data.DataLoader(torch.tensor(y_test), shuffle=True, batch_size=12)
@@ -79,5 +134,6 @@ fcn_net=FCNModel(num_signals=3, kernel_size=4, input_lengths=input_lengths)
 loss_func=torch.nn.MSELoss()
 optim_adam=torch.optim.Adam(params= fcn_net.parameters())
 
-train(fcn_net, inputs,  [x_ecg_test_loader, x_gsr_test_loader, x_inf_ppg_test_loader],  y_train_loader, y_test_loader, loss_func, optim_adam, n_epochs=10)
+train(fcn_net, [x_ecg_train_norm_loader, x_gsr_train_norm_loader, x_inf_ppg_train_norm_loader],  [x_ecg_test_norm_loader, x_gsr_test_norm_loader, x_inf_ppg_test_norm_loader],  y_train_loader, y_test_loader, loss_func, optim_adam, n_epochs=20)
+#train(fcn_net, [x_ecg_train_loader, x_gsr_train_loader, x_inf_ppg_train_loader],  [x_ecg_test_loader, x_gsr_test_loader, x_inf_ppg_test_loader],  y_train_loader, y_test_loader, loss_func, optim_adam, n_epochs=20)
 
